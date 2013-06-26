31b4772136b5f9d7c78a5fabbf111f99b2e10c95
Branislav Blaskovic <bblaskov@redhat.com>
2013-06-26 12:05:34 +0200

    Added systray

--- config.h
+++ config.h
@@ -17,6 +17,8 @@ static const char selbgcolor[]      = "#005577";
 static const char selfgcolor[]      = "#eeeeee";
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const Bool showsystray       = True;     /* False means no systray */
 static const Bool showbar           = True;     /* False means no bar */
 static const Bool topbar            = True;     /* False means bottom bar */
 
